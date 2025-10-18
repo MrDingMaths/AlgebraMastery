aaa8198ff8b4272c5abb50cb7040027427f1461c
MrDing <jsding98@outlook.com>
2025-10-18 11:48:00 +1100

    Add files via upload

--- levels/addSubtractFractionsFactorisingEasy.js
+++ levels/addSubtractFractionsFactorisingEasy.js
@@ -7,11 +7,9 @@ class AddSubtractFractionsFactorisingEasy {
         this.questions = [
             // Original textbook questions
             {problem: "\\frac{5}{x} + \\frac{2}{x+4}", answer: "\\frac{7x+20}{x(x+4)}"},
-            {problem: "\\frac{4}{x-7} + \\frac{3}{x}", answer: "\\frac{7(x-3)}{x(x-7)}"},
             {problem: "\\frac{1}{x-3} + \\frac{2}{x+5}", answer: "\\frac{3x-1}{(x-3)(x+5)}"},
             {problem: "\\frac{3}{x+3} - \\frac{2}{x-4}", answer: "\\frac{x-18}{(x+3)(x-4)}"},
             {problem: "\\frac{6}{2x-1} - \\frac{3}{x-4}", answer: "-\\frac{21}{(2x-1)(x-4)}"},
-            {problem: "\\frac{4}{x-5} + \\frac{2}{3x-4}", answer: "\\frac{2(7x-13)}{(x-5)(3x-4)}"},
             {problem: "\\frac{5}{2x-1} - \\frac{6}{x+7}", answer: "\\frac{-7x+41}{(2x-1)(x+7)}"},
             {problem: "\\frac{8}{3x-2} - \\frac{3}{1-x}", answer: "\\frac{17x-14}{(3x-2)(x-1)}"},
             {problem: "\\frac{3x+2}{3x} + \\frac{7}{12}", answer: "\\frac{19x+8}{12x}"},
@@ -34,10 +32,7 @@ class AddSubtractFractionsFactorisingEasy {
             {problem: "\\frac{4}{x} - \\frac{2}{x+6}", answer: "\\frac{2(x+12)}{x(x+6)}"},
             {problem: "\\frac{3}{2x} + \\frac{1}{x+4}", answer: "\\frac{5x+12}{2x(x+4)}"},
             {problem: "\\frac{1}{x-5} + \\frac{3}{x+2}", answer: "\\frac{4x-13}{(x-5)(x+2)}"},
-            {problem: "\\frac{6}{x+3} - \\frac{2}{x-1}", answer: "\\frac{4(x-3)}{(x+3)(x-1)}"},
             {problem: "\\frac{1}{3x} + \\frac{2}{x-2}", answer: "\\frac{7x-2}{3x(x-2)}"},
-            {problem: "\\frac{5}{x-6} - \\frac{3}{x+4}", answer: "\\frac{2(x+19)}{(x-6)(x+4)}"},
-            {problem: "\\frac{2}{x+7} + \\frac{1}{x-8}", answer: "\\frac{3(x-3)}{(x+7)(x-8)}"},
             
             // Simple coefficient variations
             {problem: "\\frac{1}{2x-1} + \\frac{1}{x+3}", answer: "\\frac{3x+2}{(2x-1)(x+3)}"},
